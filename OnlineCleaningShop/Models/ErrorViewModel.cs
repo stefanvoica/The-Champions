88f3d2b03711493e3d15833ca2bf53a2501da2ca
stefanvoica <stefan.voica04@gmail.com>
2025-05-02 23:11:52 +0300

    Clean up: added .gitignore

--- OnlineCleaningShop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OnlineCleaningShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
